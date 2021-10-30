4b9bb452884abea6081607f275e7b901104e7e1c
Sergey Obidin <player181285@gmail.com>
2021-10-30 13:26:39 +0400

    add gitignore, add code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costTicket = 15_000;
+        int costMiles = 20;
+        int bonusMiles = costTicket / costMiles;
+        System.out.println(bonusMiles);
+    }
+}
